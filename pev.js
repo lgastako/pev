23fc552625f5ca37b0564b7735fbe6f458a34610
John Evans <lgastako@gmail.com>
2015-02-23 00:27:05 -0800

    Fix double firing bug.

--- pev.js
+++ pev.js
@@ -1,173 +1,207 @@
-// Like the node.js EventEmitter but proxies all events through local storage
-// events so that cross-window event subscriptions are possible.
-//
-// The events consist of a name and an object with any details.
-function PervasiveEventEmitter(settings) {
-    var EVENT_CHANNEL_KEY = "__PEV__"
-    var EVENT_CLEARING_SIGIL = "__PEV_CLEAR_EVENT__"
+(function() {
 
-    settings = settings || {}
+    function EventEmitter(settings) {
+        settings = settings || {}
 
-    if (settings.uid) {
-        EVENT_CHANNEL_KEY = EVENT_CHANNEL_KEY + ":" + settings.uid
-    }
+        var eventListenersByEvent = {}
 
-    this.uid = settings.uid
-
-    this.eventListeners = {}
-    this.storage = settings.storage || localStorage
-
-    this._getListeners = function(event) {
-        var eventListeners = this.eventListeners[event] || []
-        this.eventListeners[event] = eventListeners
-        return eventListeners
-    }
-
-    this.on = function(event, cb) {
-        var eventListeners = this._getListeners(event)
-        eventListeners.push(cb)
-        this.eventListeners[event] = eventListeners
-        return this
-    }
-
-    this.off = function(event, cb) {
-        var eventListeners = this._getListeners(event)
-        var index = eventListeners.indexOf(cb)
-        if (index > -1) {
-            eventListeners.splice(index, 1)
+        this._listeners = function(event) {
+            var eventListeners = eventListenersByEvent[event] || []
+            eventListenersByEvent[event] = eventListeners
+            return eventListeners
         }
-        return this
+
+        this.on = function(event, cb) {
+            this._listeners(event).push(cb)
+            return this
+        }
+
+        this.off = function(event, cb) {
+            var listeners = this._listeners(event)
+            var index = listeners.indexOf(cb)
+            if (index > -1) {
+                listeners.splice(index, 1)
+            }
+            return this
+        }
+
+        this.removeAllListeners = function() {
+            eventListenersByEvent = {}
+            return this
+        }
+
+        this.listeners = function(event) {
+            // Clone the array so people can't mutate our copy
+            return this._listeners(event).splice(0)
+        }
+
+        this.listenerCount = function(event) {
+            // Use the internal _listeners(event) to avoid an array copy
+            return this._listeners(event).length
+        }
+
+        this.eachListener = function(event, f) {
+            console.log("firing " + this.listenerCount(event) + " listeners for event: " + event)
+            this._listeners(event).forEach(function(listener) {
+                try {
+                    f(listener)
+                } catch (ex) {
+                    console.log(ex)
+                }
+            })
+            return this
+        }
+
+        this._fireListeners = function(event, details) {
+            details = details || {}
+            this.eachListener(event, function(listener) {
+                try {
+                    listener(event, details)
+                } catch (ex) {
+                    console.log(ex)
+                }
+            })
+            return this
+        }
+
+        this.emit = function(event, details) {
+            console.log("emit(" + event + ", " + JSON.stringify(details) + ")")
+            return this._fireListeners(event, details)
+        }
+
+        this.addListener = this.on
+        this.removeListener = this.off
+
+        this.many = function(n, event, cb) {
+            var that = this
+
+            function listener() {
+                if (--n <= 0) {
+                    that.off(event, listener)
+                }
+                cb.apply(this, arguments)
+            }
+
+            this.on(event, listener)
+
+            return that
+        }
+
+        this.once = function(event, cb) {
+            this.many(1, event, cb)
+            return this
+        }
     }
 
-    this.removeAllListeners = function() {
+    function PervasiveEventEmitter(settings) {
+
+        settings = settings || {}
+
+        EventEmitter.call(this, settings)
+
+        var EVENT_CHANNEL_KEY = "__PEV__"
+        var EVENT_CLEARING_SIGIL = "__PEV_CLEAR_EVENT__"
+
+        if (settings.uid) {
+            EVENT_CHANNEL_KEY = EVENT_CHANNEL_KEY + ":" + settings.uid
+        }
+
+        this.uid = settings.uid
+
         this.eventListeners = {}
-        return this
-    }
+        this.storage = settings.storage || localStorage
 
-    this.listeners = function(event) {
-        var eventListeners = this._getListeners(event)
-        // Clone the array so people can't mutate our copy
-        return eventListeners.splice(0)
-    }
-
-    this.listenerCount = function(event) {
-        // Use the internal _getListeners to avoid an array copy
-        return this._getListeners(event).length
-    }
-
-    this.eachListener = function(event, f) {
-        var eventListeners = this._getListeners(event)
-        eventListeners.forEach(function(listener) {
-            try {
-                f(listener)
-            } catch (ex) {
-                console.log(ex)
-            }
-        })
-        return this
-    }
-
-    this.fireListeners = function(event, details) {
-        details = details || {}
-        this.eachListener(event, function(listener) {
-            try {
-                listener(event, details)
-            } catch (ex) {
-                console.log(ex)
-            }
-        })
-        return this
-    }
-
-    this.fireSameWindowListeners = function(event, details) {
-        // This is how we had it.  This works for listeners listerally added to
-        // the same object, but not other (distinct) objects in the same window
-        // with the same uid (which should fire together).
-        // TODO: Fix.
-        this.fireListeners(event, details)
-        return this
-    }
-
-    this.fireOtherWindowListeners = function(event, details) {
-        // We send our event and then a sigil to clear it, in case identical
-        // events are fired twice in a row.  If we go to a model where we
-        // always add a synthetic unique event ID then we could drop the song
-        // and dance with the sigil.
-        var item = {
-            event: event,
-            details: details
+        this._listeners = function(event) {
+            var eventListeners = this.eventListeners[event] || []
+            this.eventListeners[event] = eventListeners
+            return eventListeners
         }
-        console.log(JSON.stringify(item))
-        this.storage.setItem(EVENT_CHANNEL_KEY, JSON.stringify(item))
-        this.storage.setItem(EVENT_CHANNEL_KEY, EVENT_CLEARING_SIGIL)
-        return this
-    }
 
-    this.emit = function(event, details) {
-        console.log("emit(" + event + ", " + JSON.stringify(details) + ")")
-        console.log("EVENT_CHANNEL_KEY: " + EVENT_CHANNEL_KEY)
-        this.fireOtherWindowListeners(event, details)
-        this.fireSameWindowListeners(event, details)
-        return this
-    }
+        this.removeAllListeners = function() {
+            this.eventListeners = {}
+            return this
+        }
 
-    this.addListener = this.on
-    this.removeListener = this.off
+        this._fireSameWindowListeners = function(event, details) {
+            // This is how we had it.  This works for listeners listerally added to
+            // the same object, but not other (distinct) objects in the same window
+            // with the same uid (which should fire together).
+            // TODO: Fix.
+            details = details || {}
+            this.eachListener(event, function(listener) {
+                try {
+                    listener(event, details)
+                } catch (ex) {
+                    console.log(ex)
+                }
+            })
+            return this
+        }
 
-    this.many = function(n, event, cb) {
+        this._fireOtherWindowListeners = function(event, details) {
+            // We send our event and then a sigil to clear it, in case identical
+            // events are fired twice in a row.  If we go to a model where we
+            // always add a synthetic unique event ID then we could drop the song
+            // and dance with the sigil.
+            var item = {
+                event: event,
+                details: details
+            }
+            console.log(JSON.stringify(item))
+            this.storage.setItem(EVENT_CHANNEL_KEY, JSON.stringify(item))
+            this.storage.setItem(EVENT_CHANNEL_KEY, EVENT_CLEARING_SIGIL)
+            return this
+        }
+
+        this.emit = function(event, details) {
+            console.log("emit(" + event + ", " + JSON.stringify(details) + ")")
+            console.log("EVENT_CHANNEL_KEY: " + EVENT_CHANNEL_KEY)
+            this._fireOtherWindowListeners(event, details)
+            this._fireSameWindowListeners(event, details)
+            return this
+        }
+
+        function beautifyStorageArea(storageArea) {
+            if (localStorage && storageArea == localStorage) {
+                return "localStorage"
+            } else if (sessionStorage && storageArea == sessionStorage) {
+                return "sessionStorage"
+            } else {
+                return "unknown"
+            }
+        }
+
+        function beautifyStorageEvent(storageEvent) {
+            return JSON.stringify({
+                key: storageEvent.key,
+                newValue: storageEvent.newValue,
+                oldValue: storageEvent.oldValue,
+                storageArea: beautifyStorageArea(storageEvent.storageArea),
+                url: storageEvent.url
+            })
+        }
+
         var that = this
 
-        function listener() {
-            if (--n <= 0) {
-                that.off(event, listener)
-            }
-            cb.apply(this, arguments)
+        function onStorageEvent(storageEvent) {
+            if (storageEvent.storageArea != that.storage) return
+            if (storageEvent.key != EVENT_CHANNEL_KEY) return
+            if (storageEvent.newValue == EVENT_CLEARING_SIGIL) return
+
+            var val = JSON.parse(storageEvent.newValue)
+
+            var event = val.event
+            var details = val.details
+
+            that.fireListeners(event, details)
         }
 
-        this.on(event, listener)
-
-        return that
+        window.addEventListener("storage", onStorageEvent, false)
     }
 
-    this.once = function(event, cb) {
-        this.many(1, event, cb)
-        return this
+    window.PEV = {
+        EventEmitter: EventEmitter,
+        PervasiveEventEmitter: PervasiveEventEmitter
     }
 
-    function beautifyStorageArea(storageArea) {
-        if (localStorage && storageArea == localStorage) {
-            return "localStorage"
-        } else if (sessionStorage && storageArea == sessionStorage) {
-            return "sessionStorage"
-        } else {
-            return "unknown"
-        }
-    }
-
-    function beautifyStorageEvent(storageEvent) {
-        return JSON.stringify({
-            key: storageEvent.key,
-            newValue: storageEvent.newValue,
-            oldValue: storageEvent.oldValue,
-            storageArea: beautifyStorageArea(storageEvent.storageArea),
-            url: storageEvent.url
-        })
-    }
-
-    var that = this
-
-    function onStorageEvent(storageEvent) {
-        if (storageEvent.storageArea != that.storage) return
-        if (storageEvent.key != EVENT_CHANNEL_KEY) return
-        if (storageEvent.newValue == EVENT_CLEARING_SIGIL) return
-
-        var val = JSON.parse(storageEvent.newValue)
-
-        var event = val.event
-        var details = val.details
-
-        that.fireListeners(event, details)
-    }
-
-    window.addEventListener("storage", onStorageEvent, false)
-}
+})()
--- tests/tests.js
+++ tests/tests.js
@@ -1,179 +1,216 @@
-function Example() {
-    PervasiveEventEmitter.call(this)
+function ExampleEmitter() {
+    PEV.EventEmitter.call(this)
 }
 
+function ExamplePervasiveEmitter() {
+    PEV.PervasiveEventEmitter.call(this)
+}
+
+ALL_EMITTERS = [ExampleEmitter, ExamplePervasiveEmitter]
+
 
 QUnit.test(
-    "'on' causes listener to be added.",
+    "'on' causes listener to be added in all emitters.",
 
     function(assert) {
-        var example = new Example()
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
 
-        example.on("foo", "bar")
+            emitter.on("foo", "bar")
 
-        // Ideally we wouldn't check for internal state, BFN...
-        assert.deepEqual(example.eventListeners, {
-            "foo": ["bar"]
+            assert.deepEqual(emitter.listeners("foo"), ["bar"])
         })
     }
 )
 
 
 QUnit.test(
-    "'off' causes listener to be removed.",
+    "'off' causes listener to be removed in all emitters.",
 
     function(assert) {
-        var example = new Example()
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
 
-        example.on("foo", "bar")
-        example.off("foo", "bar")
+            emitter.on("foo", "bar")
+            emitter.off("foo", "bar")
 
-        assert.deepEqual(example.eventListeners, {
-            "foo": []
+            assert.deepEqual(emitter.listeners("foo"), [])
         })
     }
 )
 
 
 QUnit.test(
-    "'removeAllListeners' causes all listeners to be removed.",
+    "'removeAllListeners' causes all listeners to be removed in all emitters.",
 
     function(assert) {
-        var example = new Example()
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
 
-        example.on("foo", "bar")
-        example.on("baz", "bif")
+            emitter.on("foo", "bar")
+            emitter.on("baz", "bif")
 
-        example.removeAllListeners()
+            emitter.removeAllListeners()
 
-        assert.deepEqual(example.eventListeners, {})
-    }
-)
-
-
-QUnit.test(
-    "'listeners' returns the list of listeners for any event",
-
-    function(assert) {
-        var example = new Example()
-
-        example.on("foo", "bar")
-        example.on("foo", "baz")
-        example.on("bif", "bam")
-
-        assert.deepEqual(example.listeners("foo"), ["bar", "baz"])
-        assert.deepEqual(example.listeners("bif"), ["bam"])
-    }
-)
-
-
-QUnit.test(
-    "'listenerCount' works",
-
-    function(assert) {
-        var example = new Example()
-
-        example.on("foo", "bar")
-        example.on("foo", "baz")
-        example.on("bif", "bam")
-
-        assert.equal(example.listenerCount("foo"), 2)
-        assert.equal(example.listenerCount("bif"), 1)
-        assert.equal(example.listenerCount("baz"), 0)
-    }
-)
-
-
-QUnit.test(
-    "'once' fires once and only once",
-
-    function(assert) {
-        var example = new Example()
-        var calls = 0
-
-        example.emit("foo")
-        assert.equal(calls, 0)
-
-        example.once("foo", function() {
-            calls++
+            assert.deepEqual(emitter.listeners("foo"), [])
+            assert.deepEqual(emitter.listeners("bar"), [])
         })
-
-        example.emit("foo")
-        assert.equal(calls, 1)
-
-        example.emit("foo")
-        assert.equal(calls, 1)
     }
 )
 
 
 QUnit.test(
-    "'many' fires the appropriate number of times",
+    "'listeners' returns the list of listeners for any event in all emitters.",
 
     function(assert) {
-        var example = new Example()
-        var calls = 0
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
 
-        example.emit("foo")
-        assert.equal(calls, 0)
+            emitter.on("foo", "bar")
+            emitter.on("foo", "baz")
+            emitter.on("bif", "bam")
 
-        example.many(2, "foo", function() {
-            calls++
+            assert.deepEqual(emitter.listeners("foo"), ["bar", "baz"])
+            assert.deepEqual(emitter.listeners("bif"), ["bam"])
         })
-
-        example.emit("foo")
-        assert.equal(calls, 1)
-
-        example.emit("foo")
-        assert.equal(calls, 2)
-
-        example.emit("foo")
-        assert.equal(calls, 2)
     }
 )
 
 
 QUnit.test(
-    "'emit' causes listeners in the SAME WINDOW to receive events",
+    "'listenerCount' works in all emitters.",
 
     function(assert) {
-        var example = new Example()
-        var foos = []
-        var done = assert.async()
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
 
-        example.emit("foo", {
-            which: 0
+            emitter.on("foo", "bar")
+            emitter.on("foo", "baz")
+            emitter.on("bif", "bam")
+
+            assert.equal(emitter.listenerCount("foo"), 2)
+            assert.equal(emitter.listenerCount("bif"), 1)
+            assert.equal(emitter.listenerCount("baz"), 0)
         })
-
-        example.on("foo", function(event, details) {
-            console.log("Callback for foo: " + event + ", details => " + details)
-            foos.push(details)
-        })
-
-        example.emit("foo", {
-            which: 1
-        })
-
-        console.log("event emitted.")
-
-        setTimeout(function() {
-            console.log("checking in setTimeout")
-            assert.deepEqual(foos, [{which: 1}])
-            done()
-        }, 10)
     }
 )
 
 
 QUnit.test(
-    "Private event emitters known their own unique ID.",
+    "'once' fires once and only once for all emitters.",
 
     function(assert) {
-        p1 = new PervasiveEventEmitter({
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
+            var calls = 0
+
+            emitter.emit("foo")
+            assert.equal(calls, 0)
+
+            emitter.once("foo", function() {
+                calls++
+            })
+
+            emitter.emit("foo")
+            assert.equal(calls, 1)
+
+            emitter.emit("foo")
+            assert.equal(calls, 1)
+        })
+    }
+)
+
+
+QUnit.test(
+    "'many' fires the appropriate number of times in the local window for all emitters.",
+
+    function(assert) {
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
+            var calls = 0
+
+            var cname = constructor.name + " => "
+
+            emitter.emit("foo")
+            assert.deepEqual(cname + calls, cname + 0)
+
+            emitter.many(2, "foo", function() {
+                calls++
+            })
+
+            emitter.emit("foo")
+            assert.equal(cname + calls, cname + 1)
+
+            emitter.emit("foo")
+            assert.equal(cname + calls, cname + 2)
+
+            emitter.emit("foo")
+            assert.equal(cname + calls, cname + 2)
+
+
+            // emitter.emit("foo")
+            // assert.deepEqual({cname: cname, calls: calls}, {cname: cname, calls: 0})
+
+            // emitter.many(2, "foo", function() {
+            //     calls++
+            // })
+
+            // emitter.emit("foo")
+            // assert.equal(cname + calls, cname + 1)
+
+            // emitter.emit("foo")
+            // assert.equal(cname + calls, cname + 2)
+
+            // emitter.emit("foo")
+            // assert.equal(cname + calls, cname + 2)
+        })
+    }
+)
+
+
+// QUnit.test(
+//     "'emit' causes listeners in the SAME WINDOW to receive events in both",
+
+//     function(assert) {
+//         [ExamplePervasiveEmitter, ExamplePervasiveEmitter].forEach(function(constructor) {
+//             var emitter = new constructor()
+//             var foos = []
+//             var done = assert.async()
+
+//             emitter.emit("foo", {
+//                 which: 0
+//             })
+
+//             emitter.on("foo", function(event, details) {
+//                 console.log("Callback for foo: " + event + ", details => " + details)
+//                 foos.push(details)
+//             })
+
+//             emitter.emit("foo", {
+//                 which: 1
+//             })
+
+//             console.log("event emitted.")
+
+//             setTimeout(function() {
+//                 console.log("checking in setTimeout")
+//                 assert.deepEqual(foos, [{which: 1}])
+//                 done()
+//             }, 10)
+//         })
+//     }
+// )
+
+
+QUnit.test(
+    "Private PervasiveEventEmitters known their own unique ID.",
+
+    function(assert) {
+        p1 = new PEV.PervasiveEventEmitter({
             uid: "uid1"
         })
 
-        p2 = new PervasiveEventEmitter({
+        p2 = new PEV.PervasiveEventEmitter({
             uid: "uid2"
         })
 
@@ -187,10 +224,10 @@ QUnit.test(
     "Events on private emitters are private (locally).",
 
     function(assert) {
-        var publicEvents = new PervasiveEventEmitter()
-        var privateEvents1 = new PervasiveEventEmitter({uid: "private-1"})
-        var privateEvents1a = new PervasiveEventEmitter({uid: "private-1"})
-        var privateEvents2 = new PervasiveEventEmitter({uid: "private-2"})
+        var publicEvents = new PEV.PervasiveEventEmitter()
+        var privateEvents1 = new PEV.PervasiveEventEmitter({uid: "private-1"})
+        var privateEvents1a = new PEV.PervasiveEventEmitter({uid: "private-1"})
+        var privateEvents2 = new PEV.PervasiveEventEmitter({uid: "private-2"})
 
         var recordedEvents = {}
 
@@ -268,25 +305,39 @@ QUnit.test(
 
 
 QUnit.test(
-    "Chainability of various PervasiveEventEmitter methods",
+    "Chainability of common methods in all emitters.",
 
     function(assert) {
-        var p = new PervasiveEventEmitter()
-        var cb = function() {}
+        ALL_EMITTERS.forEach(function(constructor) {
+            var emitter = new constructor()
+            var cb = function() {}
 
-        assert.equal(p,
-                     p
-                     .on("foo", cb)
-                     .off("foo", cb)
-                     .eachListener(cb)
-                     .fireListeners("bar")
-                     .fireSameWindowListeners("bar")
-                     .fireOtherWindowListeners("bar")
-                     .emit("baz")
-                     .addListener("bim", cb)
-                     .removeListener("bim", cb)
-                     .once("bop", cb)
-                     .many(3, "bop", cb)
-                     .removeAllListeners())
+            assert.equal(emitter,
+                         emitter
+                         .on("foo", cb)
+                         .off("foo", cb)
+                         .eachListener(cb)
+                         .emit("baz")
+                         .addListener("bim", cb)
+                         .removeListener("bim", cb)
+                         .once("bop", cb)
+                         .many(3, "bop", cb)
+                         .removeAllListeners())
+
+            if (constructor == ExampleEmitter) {
+
+                assert.equal(emitter,
+                             emitter
+                             ._fireListeners("bar"))
+
+            } else if (constructor == ExamplePervasiveEmitter) {
+
+                assert.equal(emitter,
+                             emitter
+                             ._fireSameWindowListeners("bar")
+                             ._fireOtherWindowListeners("bar"))
+
+            }
+        })
     }
 )
